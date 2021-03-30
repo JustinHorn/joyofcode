45d95bd1eaf6691d6ed4408a25bd8929c57a68f6
y0092888 <ju-horn@web.de>
2021-03-30 20:52:43 +0200

    filter & lined& cached now in one row

--- client/src/pages/MainPage/index.js
+++ client/src/pages/MainPage/index.js
@@ -1,13 +1,20 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import Feed from "component/Feed";
 
 import UserContext from "context";
+import { useLocation, useHistory } from "react-router-dom";
 
 import TagInput from "component/forms/Inputs/TagInput";
 
 import styles from "./mainpage.module.css";
+import ProjectLayoutContext from "context/ProjectLayout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import ToggleLinedCached from "component/ToggleLinedCached";
+function useQuery() {
+  const location = useLocation();
+  return new URLSearchParams(location.search);
+}
 
 const Mainpage = () => {
   const { user } = useContext(UserContext);
@@ -28,22 +35,61 @@ const Mainpage = () => {
     return true;
   };
 
+  const query = useQuery();
+
+  const queryLined = query.get("lined");
+
+  let initLined = false;
+
+  if (queryLined && (queryLined === "true" || queryLined === "false")) {
+    initLined = queryLined === "true";
+  }
+
   const setValue = (x) => {
     setFilters({ ...filters, value: x });
   };
 
+  const [lined, setLined] = useState(!!initLined);
+
+  const history = useHistory();
+  useEffect(() => {
+    if (lined) {
+      query.set("lined", "true");
+    } else {
+      query.set("lined", "false");
+    }
+    history.replace(window.location.pathname + "?" + query.toString());
+  }, [lined]);
+
   return (
     <div className={styles.main}>
       <div className={styles.greet}>{<h2>Hello {user?.name} </h2>}</div>
-      <TagInput
-        className={styles.searchbar}
-        formValue={filters}
-        setSpecificFormValue={setValue}
-      />
+
       <div className={styles.feed}>
-        <ToggleLinedCached>
+        <div className={styles.searchbarLayout}>
+          <TagInput
+            className={styles.searchbar}
+            formValue={filters}
+            setSpecificFormValue={setValue}
+          />
+          <div>
+            <button
+              className={(lined ? "" : "not-selected") + " iconButton"}
+              onClick={() => setLined(true)}
+            >
+              <FontAwesomeIcon icon={["fas", "bars"]}> h</FontAwesomeIcon>
+            </button>
+            <button
+              className={(lined ? "not-selected" : "") + " iconButton"}
+              onClick={() => setLined(false)}
+            >
+              <FontAwesomeIcon icon={["fas", "th"]}> h</FontAwesomeIcon>
+            </button>
+          </div>
+        </div>
+        <ProjectLayoutContext.Provider value={{ lined }}>
           <Feed filter={filter} />
-        </ToggleLinedCached>
+        </ProjectLayoutContext.Provider>
       </div>
     </div>
   );
