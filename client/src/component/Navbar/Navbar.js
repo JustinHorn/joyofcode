4b433e7cc089c7975a8bf5c85ded285c0184a3f4
y0092888 <ju-horn@web.de>
2021-04-10 08:36:29 +0200

    made navbar not sticky

--- client/src/component/Navbar/Navbar.js
+++ client/src/component/Navbar/Navbar.js
@@ -17,32 +17,35 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={"box-shadow " + styles.nav}>
-      <div className={styles.pages}>
-        <NavLink to="/">
-          <button className={isSelected("/")}>Home </button>
-        </NavLink>
-
-        {user && (
-          <NavLink to={"/user/" + user.id}>
-            <button className={isSelected("/user/" + user.id)}> Me</button>
+    <div className="">
+      <nav className={"box-shadow " + styles.nav}>
+        <div className={styles.pages}>
+          <NavLink to="/">
+            <button className={isSelected("/")}>Home </button>
           </NavLink>
-        )}
-      </div>
 
-      <div className={styles.headline}>
-        <h1>Joy of Code </h1>
-      </div>
+          {user && (
+            <NavLink to={"/user/" + user.id}>
+              <button className={isSelected("/user/" + user.id)}> Me</button>
+            </NavLink>
+          )}
+        </div>
 
-      <div className={styles.actions}>
-        {user && (
-          <NavLink to="/post">
-            <button className={isSelected("/post")}> Post</button>
-          </NavLink>
-        )}
-        <AuthenticationButtons />
-      </div>
-    </nav>
+        <div className={styles.headline}>
+          <h1>Joy of Code </h1>
+        </div>
+
+        <div className={styles.actions}>
+          {user && (
+            <NavLink to="/post">
+              <button className={isSelected("/post")}> Post</button>
+            </NavLink>
+          )}
+          <AuthenticationButtons />
+        </div>
+      </nav>
+      <br />
+    </div>
   );
 };
 
